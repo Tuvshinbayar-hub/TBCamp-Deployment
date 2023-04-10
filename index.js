c2e9a365db3be1c6e068f3169aa0226f46a82a3b
Tuvshuu <tuvshuu@mirai.mn>
2023-04-10 13:13:29 +0800

    no longer secure on cookies

--- index.js
+++ index.js
@@ -89,8 +89,8 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     name: 'sessionID',
-    //httpOnly: true,
-    secure: true, //For now it's commented 'cause local host is not considered as
+    httpOnly: true,
+    //secure: true, //For now it's commented 'cause local host is not considered as
     //https or http secure. So, it does not work in local host. 
     expires: Date.now() + 1000 * 3600 * 24 * 7,
     maxAge: 1000 * 3600 * 24 * 7
